b3d18562112892c09ec353eec005313d474fbcdf
Oleksandr Tarasenko <aleksandr.tarasenko@gmail.com>
2026-05-20 16:31:33 +0200

    Introduce Service layer - DeviceService interface

--- src/main/java/com/example/device/service/DeviceService.java
+++ src/main/java/com/example/device/service/DeviceService.java
@@ -1,12 +1,27 @@
 package com.example.device.service;
 
 import com.example.device.model.Device;
+import com.example.device.model.DeviceState;
+import com.example.device.service.dto.DeviceCreateRequest;
 import com.example.device.service.dto.DeviceUpdateRequest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DeviceService {
-    @Transactional
     Device updateDevice(UUID id, DeviceUpdateRequest updateRequest);
+
+    Device createDevice(DeviceCreateRequest deviceCreateRequest);
+
+    void deleteDevice(UUID id);
+
+    Device findById(UUID id);
+
+    List<Device> findByBrand(String brand);
+
+    List<Device> findByState(DeviceState state);
+
+    Page<Device> findAll(PageRequest pageRequest);
 }
